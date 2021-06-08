33381150d6c68ca9d0527048fb1eb38bbadbebae
Sir-Modernisation <david.strudwick1301@gmail.com>
2021-06-08 11:24:01 +1000

    Crosshair among other things

--- Caff Studios Project/Assets/Scripts/PlayerGun.cs
+++ Caff Studios Project/Assets/Scripts/PlayerGun.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class PlayerGun : MonoBehaviour
 {
-    //Scrip to control the players gun - initially getting raycasting shots working, Then will also be responsible for Animations, Tracking the number of rounds fired, Scope,
-    // Start is called before the first frame update
+    //Script to control the players gun - initially getting raycasting shots working, Then will also be responsible for Animations, Tracking the number of rounds fired, Scope, etc
+
+    int mag;
     void Start()
     {
-        
+        mag = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //-Section: Managing wether the gun can be fired or needs to be reloaded-
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(mag != 0)
+            {
+                Shoot();
+            }
+            else
+            {
+                print("Click!");
+            }
+
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            if (mag != 5)
+            {
+                {
+                    print("Reloading:");
+                    for (int i=mag; i<5; ++i)
+                    {
+                        print("chk");
+                    }
+                    mag = 5;
+                }
+            }
+            else
+            {
+                print("full");
+            }
+        }
     }
+    //--------------------------------------------------------------
+    void Shoot()
+    {
+        print("Bang!");
+        mag = mag - 1;
+        //playanimation shoot or something
+        //Raycast here
+    }
+    //-------------------------------------------------------------
+
+
 }
